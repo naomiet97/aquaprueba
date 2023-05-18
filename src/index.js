18a25e5486ad2d4ac1a175278f50b7f2e8801f71
Naomi Elvira <naomiet97@gmail.com>
2023-05-18 19:32:06 +0200

    Actualizo repo con blob1.png. Naomi

---
+++ src/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const fishContainer = document.getElementById('fish-container');
+    const fishImages = ['fish1.png', 'fish2.png', 'fish3.png', 'fish4.png']; // Agrega aquí las rutas de tus imágenes de peces
+  
+    const numFish = 10; // Número de peces que deseas mostrar
+  
+    for (let i = 0; i < numFish; i++) {
+      const fish = document.createElement('img');
+      fish.src = fishImages[Math.floor(Math.random() * fishImages.length)];
+      fish.classList.add('fish');
+      fish.style.left = `${Math.random()
+  
